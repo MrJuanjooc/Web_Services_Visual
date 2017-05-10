bbe664d92f4816677ed46c2f16e672574e3ca770
JUAN <JUAN@DESKTOP-SJ17NKE>
2017-05-09 19:50:45 -0500

    Web Service Visual

---
+++ WCF_TL_Solution/WCF_Mi_Servicio/IServicio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Mi_Servicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicio" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicio
+    {
+        [OperationContract]
+        int Get_Suma(int Numero1, int Numero2);
+
+    }
+}
---
+++ WCF_TL_Solution/WCF_Mi_Servicio/Servicio.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Mi_Servicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Servicio" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Servicio.svc o Servicio.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Servicio : IServicio
+    {
+        public int Get_Suma(int Numero1, int Numero2)
+        {
+            int Resultado = (Numero1 + Numero2);
+            return Resultado;
+        }
+    }
+}
